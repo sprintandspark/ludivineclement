5b9fa1c9d4295a3e7e502fe4a249367ec2db844f
sprintandspark <ludiclem@gmail.com>
2026-03-18 10:35:45 +0100

    Move base path to correct vite config file
    Remove base path configuration for Vitest.

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: '/ludivineclement/',
   plugins: [react()],
   test: {
     environment: "jsdom",
