3f514e681840ea8c472c70af338f77dd4a7d4d13
sprintandspark <ludiclem@gmail.com>
2026-09-18 16:51:35 +0200

    Add rosa color variable to Tailwind configuration
    Add 'rosa' color variable with alpha support to Tailwind config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -22,6 +22,7 @@ export default {
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
+        rosa: 'hsl(var(--rosa) / <alpha-value>)',
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
